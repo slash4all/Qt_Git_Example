2e650cfae0493fda1a86c56cd053ddf7e8479697
vignoz <vignoz96@gmail.com>
2022-02-09 19:28:26 +0100

    added scene,img,view and checkable to btn

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,11 +6,18 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    scene = new QGraphicsScene(0,0, 190, 143);
+//    img = new QPixmap(":/ass_img/res/nice_ass.jpg");
+    scene->addPixmap(QPixmap(":/ass_img/res/nice_ass.jpg"));
+
+    ui->graphicsView->setScene(scene);
+    ui->graphicsView->show();
 }
 
 MainWindow::~MainWindow()
 {
-
+   delete scene;
    delete ui;
 }
 
@@ -19,5 +26,14 @@ MainWindow::~MainWindow()
 void MainWindow::on_kick_btn_clicked()
 {
         ui->kicked_lbl->setText(ui->kicked_lbl->text()+" ouch!");
+}
+
+void MainWindow::on_kick_btn_toggled(bool checked)
+{
+    if (checked)
+        scene->addPixmap(QPixmap(":/ass_img/res/bad_ass.jpg"));
+    else
+        scene->addPixmap(QPixmap(":/ass_img/res/nice_ass.jpg"));
+
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QGraphicsScene>
+#include <QGraphicsPixmapItem>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -19,7 +21,14 @@ private slots:
 
     void on_kick_btn_clicked();
 
+    void on_kick_btn_toggled(bool checked);
+
 private:
     Ui::MainWindow *ui;
+
+    QGraphicsScene *scene;
+
+    QPixmap *img;
+
 };
 #endif // MAINWINDOW_H
